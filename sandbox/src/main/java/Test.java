237a3e9e1c2c87716548c58e39cc9217a060e8b6
nikitindmytro <nikitin.dmitry.a@gmail.com>
2018-02-15 15:33:52 +0200

    functions added

--- sandbox/src/main/java/Test.java
+++ sandbox/src/main/java/Test.java
@@ -4,6 +4,21 @@ public class Test {
 	{
 
 		System.out.println("Hello, World!");
-		
+
+		double a = 5;
+        System.out.println("Площадь квадрата, состороной " + a + " = " + area(a));
+
+        double x = 3;
+        double y = 6;
+        System.out.println("Площадь прямоугольника, со сторонами " + x + " и " + y + " = " + area(x,y));
 	}
+
+	public static double area(double a){
+		return a*a;
+	}
+
+    public static double area(double a,double b){
+        return a*b;
+    }
+
 }
